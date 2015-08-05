017fddc85b4f4bbe27c97b39c9719b7f7a271a75
leemyeongho <l.m.myeongho.lee@gmail.com>
2015-08-05 08:37:59 +0200

    ユーザー名登録を実装

---
+++ db/migrate/20150805060836_add_columns_to_user.rb
@@ -0,0 +1,6 @@
+class AddColumnsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_column :users, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804095753) do
+ActiveRecord::Schema.define(version: 20150805060836) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20150804095753) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
